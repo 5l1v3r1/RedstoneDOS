76f53abe7dd8703f7734b608f255d3fae21161b1
CosmoXDD <benjaminsalazarballadares@gmail.com>
2020-03-18 16:04:21 -0300

    Kernel: Added input_string() & Ended CMD.

--- src/kernel/utils/cmd.asm
+++ src/kernel/utils/cmd.asm
@@ -1,6 +1,7 @@
 %include "screen.asm"
 %include "string.asm"
 %include "basic.asm"
+%include "keyboard.asm"
 %include "fs.asm"
 
 command_line:
@@ -13,6 +14,106 @@ command_line:
     call print_str
 
 cmd_main:
+    mov di, input
+    mov al, 0
+    mov cx, 256
+    rep stosb
+
+    mov di, command
+    mov cx, 32
+    rep stosb
+
+    mov si, program
+    call print_str
+
+    call print_ln
+
+    mov ax, input
+    call input_string
+
+    call print_ln
+
+    mov ax, input
+    
+    mov ax, input
+    call string_chomp
+
+    mov si, input
+    cmp byte [si], 0
+    je cmd_main
+
+    mov si, input
+    mov alm ' '
+    call string_tokenize
+
+    mov word [param_list], di
+
+    mov si, input
+    mov di, command
+    call string_copy
+
+    mov ax, input
+    call string_uppercase
+
+    mov si, input
+
+    mov di, exit_str
+    call str_compare
+    jc near exit
+
+    mov di, help_str
+    call str_compare
+    jc near print_help
+
+    mov di, cls_str
+    call str_compare
+    jc near clear_screen
+
+    mov di, dir_str
+    call str_compare
+    jc near dir_list
+
+    mov di, ver_str
+    call str_compare
+    jc near print_ver
+
+    mov di, rm_str
+    call str_compare
+    jc near rm_file
+
+    mov di, exit_str
+    call str_compare
+    jc near exit
+
+    mov di, cd_str
+    call str_compare
+    jc near
+
+bin_file:
+    mov ax, command
+    mov bx, 0
+    mov cx, 32768
+    call os_load_file
+    jc total_fail
+
+excecute_bin:
+    mov si, command
+    mov di, kern_file_string
+    mov cx, 6
+    call str_compare
+    jc no_kernel_allowed_msg
+    call print_str
+
+    mov ax, 0
+    mov bx, 0
+    mov cx, 0
+    mov dx, 0
+    mov word si, [param_list]
+    mov di, 0
+    
+    call 32768
+
+    jmp cmd_main
 
 basic_file:
     mov ax, command
@@ -25,6 +126,44 @@ basic_file:
     mov word si, [param_list]
     call os_run_basic
     jmp cmd_main
+
+not_extension:
+    mov ax, command
+    call strlen
+
+    mov si, command
+    add si, ax
+
+    mov byte [si], '.'
+    mov byte [si+1], 'b'
+    mov byte [si+2], 'i'
+    mov byte [si+3], 'n'
+    mov byte [si+4], 0
+
+    mov ax, command
+    mov bx, 0
+    mov cx, 32768
+    call os_load_file
+    jc try_basic_exit
+
+    jmp excecute_bin
+
+try_basic_exit:
+    mov ax, command
+    call strlen
+
+    mov si, command
+    add si, ax
+    sub si, 4
+
+    mov byte [si], '.'
+    mov byte [si+1], 'b'
+    mov byte [si+2], 'a'
+    mov byte [si+3], 's'
+    mov byte [si+4], 0
+
+    jmp basic_file
+
 
 total_fail:
     mov si, invalid_msg
@@ -46,7 +185,7 @@ print_ver:
     call print_str
     jmp cmd_main
 
-; -----------------------------
+; -------------------------------
 dir_list:
     mov cx, 0
 
@@ -73,7 +212,34 @@ dir_list:
     call print_ln
     jmp cmd_main
 ; -------------------------------
+rm_file:
+    mov word si, [param_list]
+    call string_parse
+    cmp ax, 0
+    jne .filename_provided
 
+    mov si, nofilename_msg
+    call print_str
+    jmp cmd_main
+
+.filename_provided:
+    call os_remove_file
+    jc .failure
+
+    mov si, success_msg
+    call print_str
+    mov si, ax
+    call print_str
+    call print_ln
+    jmp cmd_main
+
+.failure:
+    mov si, .failure_msg
+    call print_str
+    jmp cmd_main
+
+    .failure_msg db 'The file could not be deleted, possibly it does not exist or its writing is protected.'
+; -------------------------------
 
 exit:
     ret
@@ -87,6 +253,8 @@ input times 256 db 0
 command times 32 db 0
 dirlist times 1024 db 0
 
+param_list dw 0
+
 bas_ext db '.bas', 
 
 ; Tokens
@@ -97,3 +265,10 @@ dir_str db 'dir', 0
 cd_str db 'cd', 0
 rm_str db 'rm', 0
 cls_str db 'cls', 0
+
+nofilename_msg db 'You have not entered a file name! Example: rm hello.txt', 13, 10, 0
+success_msg db 'File successfully deleted!'
+
+no_kernel_allowed_msg db 'The kernel cannot run this .bin file, possibly it is corrupt.'
+
+kern_file_string db 'RDOS', 0
--- src/kernel/utils/string.asm
+++ src/kernel/utils/string.asm
@@ -1,3 +1,6 @@
+%include "keyboard.asm"
+
+; ------------------------------------------
 print_str:
     mov ah, 0Eh
 
@@ -13,3 +16,289 @@ print_str:
 
     times 510-($-$$) db 0
     dw 0xAA55
+
+; ------------------------------------------
+input_string:
+    pusha
+
+    mov di, ax
+    mov cx, 0
+
+.more:
+    call wait_for_key
+
+    cmp al, 13
+    je .done
+
+    cmp al, 8
+    je .backspace
+
+    cmp al, '  '
+    jb .more
+
+    cmp al, '~'
+    ja .more
+
+    jmp .nobackspace
+
+.backspace:
+    cmp cx, 0
+    je .more
+
+    pusha
+    mov ah, 0Eh
+    mov al, 8
+    int 10h
+    mov al, 32
+    int 10h
+    mov al, 8
+    int 10h
+    popa
+
+    dec di
+    dec cx
+
+    jmp .more
+
+.nobackspace:
+    pusha
+    mov ah, 0Eh
+    int 10h
+
+    popa
+    
+    stosb
+    inc cx
+    cmp cx, 254
+    jae near .done
+
+    jmp near .more
+
+.done:
+    mov ax, 0
+    stosb
+
+    popa
+    ret
+; ------------------------------------------
+string_tokenize:
+    push si
+
+.next_char:
+    cmp byte [si], al
+    je .return_token
+    cmp byte [si], 0
+    jz .no_more
+    inc si
+    jmp .next_char
+
+.return_token:
+    mov byte [si], 0
+    inc si
+    mov di, si
+    pop si
+    ret
+
+.no_more:
+    mov di, 0
+    pop si
+    ret
+
+; ------------------------------------------
+string_chomp:
+    pusha
+
+    mov dx, ax
+
+    mov di, ax
+    mov cx, 0
+
+.keepcounting:
+    cmp byte [di], ' '
+    jne .counted
+    inc cx
+    inc di
+    jmp .keepcounting
+
+.counted:
+    cmp cx, 0
+    je .finished_copy
+
+    mov si, di
+    mov di, dx
+
+.keep_copying:
+    mov al, [si]
+    mov [di], al
+    cmp al, 0
+    je .finished_copy
+    inc si
+    inc di
+    jmp .keep_copying
+
+.finished_copy:
+    mov ax, dx
+
+    call strlen
+    cmp ax, 0
+    je .done
+
+    mov si, dx
+    add si, ax
+
+.more:
+    dec si
+    cmp byte [si], ' '
+    jne .done
+    mov byte [si], 0
+    jmp .more
+
+.done:
+    popa
+    ret
+; ------------------------------------------
+strlen:
+    pusha
+
+    mov bx, ax
+
+    mov cx, 0
+
+.more:
+    cmp byte [bx], 0
+    je .done
+    inc bx
+    inc cx
+    jmp .more
+
+.done:
+    mov word[.tmp_counter], cx
+    popa
+
+    mov ax, [.tmp_counter]
+    ret
+
+    .tmp_counter dw 0
+; ------------------------------------------
+string_copy:
+    pusha
+
+.more:
+    mov al, [si]
+    mov [di], al
+    inc si
+    inc di
+    cmp byte al, 0
+    jne .more
+
+.done:
+    popa
+    ret
+; ------------------------------------------
+string_uppercase:
+    pusha
+
+    mov si, ax
+
+.more:
+    cmp byte [si], 0
+    je .done
+
+    cmp byte [si], 'a'
+    jb .noatoz
+    cmp byte [si], 'z'
+    ja .noatoz
+
+    sub byte [si], 20h
+
+    inc si
+    jmp .more
+
+.noatoz:
+    inc si
+    jmp .more
+
+.done:
+    popa
+    ret
+; ------------------------------------------
+str_compare:
+    pusha
+
+.more:
+    mov al, [si]
+    mov bl, [di]
+
+    cmp al, bl
+    jne .not_same
+
+    cmp al, 0
+    je .terminated
+
+    inc si
+    inc di
+    jmp .more
+
+.not_same:
+    popa
+    clc
+    ret
+
+.terminated:
+    popa
+    stc
+    ret
+; ------------------------------------------
+string_parse:
+    push si
+
+    mov ax, si
+
+    mov bx, 0
+    mov cx, 0
+    mov dx, 0
+
+    push ax
+
+.loop1:
+	lodsb
+	cmp al, 0
+	je .finish
+	cmp al, ' '
+	jne .loop1
+	dec si
+	mov byte [si], 0
+
+	inc si
+	mov bx, si
+
+.loop2:
+	lodsb
+	cmp al, 0
+	je .finish
+	cmp al, ' '
+	jne .loop2
+	dec si
+	mov byte [si], 0
+
+	inc si
+	mov cx, si
+
+.loop3:
+	lodsb
+	cmp al, 0
+	je .finish
+	cmp al, ' '
+	jne .loop3
+	dec si
+	mov byte [si], 0
+
+	inc si
+	mov dx, si
+
+.finish:
+	pop ax
+
+	pop si
+	ret
+; ------------------------------------------
