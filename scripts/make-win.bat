c2c64309761fee913c3c72726b84f491ba13956d
CosmoXDD <benjaminsalazarballadares@gmail.com>
2020-03-22 17:26:48 -0300

    Scripts: Fixed windows build script.

--- scripts/make-win.bat
+++ scripts/make-win.bat
@@ -1,12 +1,26 @@
+@echo off
+
 cd ../
-cd src/
-dir
-cd kernel
-dir
-nasm -O0 -f bin -o RDOS.bin kernel.asm -ic:\utils\
-dir
-cd bootloader
-dir
-nasm -O0 -f bin -o bootloader.bin bootloader.asm
-cd ../../../
+mkdir disk_images
+cd scripts/
+
+echo RedstoneDOS Buld Script (WINDOWS)
+
+echo Assembling bootloader...
+nasm -O0 -f bin -o bootloader.bin ../src/kernel/bootloader/bootloader.asm
+
+echo Assembling kernel...
+nasm -O0 -f bin -o RDOS.bin ../src/kernel/kernel.asm
+
+echo Adding bootsector to disk image...
+imdisk -a -f disk_images\RedstoneDOS.flp -s 1440K -m b:
+
+echo Copying kernel and applications to disk image...
+copy RDOS.bin b:\
+
+echo Dismounting disk image...
+imdisk -D -m B:
+
+echo Done!
+
 PAUSE
